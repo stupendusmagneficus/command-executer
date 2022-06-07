a6b15c342eb9d41d9cd3451bb73cf47fe3c175f1
YD <yahordunayeu@gmail.com>
2022-06-07 20:36:40 +0200

    Add input processing. Installed prettier.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
+import { PromptService } from './core/prompt/prompt.service'
+
 export class App {
-    run() {
-        console.log('Start project')
-    }
+  async run() {
+    const res = await new PromptService().input<number>('Number', 'number')
+    console.log({ res })
+  }
 }
 
-const app = new App();
+const app = new App()
 app.run()
