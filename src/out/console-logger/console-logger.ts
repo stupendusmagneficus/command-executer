2e9228f6037ba70b9b731f93ae8867d91e8c06a4
YD <yahordunayeu@gmail.com>
2022-06-07 22:14:53 +0200

    Add ConsoleLogger. Implement pattern Singleton.

---
+++ src/out/console-logger/console-logger.ts
@@ -0,0 +1,23 @@
+import { IStreamLogger } from '../../core/handlers/stream-logger.interface'
+
+export class ConsoleLogger implements IStreamLogger {
+  private static logger: ConsoleLogger
+  public static getInstance() {
+    if (!ConsoleLogger.logger) {
+      ConsoleLogger.logger = new ConsoleLogger()
+    }
+    return ConsoleLogger.logger
+  }
+
+  end(): void {
+    console.log('End')
+  }
+
+  error(...args: any[]): void {
+    console.log(...args)
+  }
+
+  log(...args: any[]): void {
+    console.log(...args)
+  }
+}
