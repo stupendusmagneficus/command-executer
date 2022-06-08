bfcc54eeac236d320cdd7497dfcedd068416b1d0
YD <yahordunayeu@gmail.com>
2022-06-08 22:43:55 +0200

    Implement template CommandExecutor method.

---
+++ src/core/executor/command.executor.ts
@@ -0,0 +1,24 @@
+import { ICommandExecutor } from './command.types';
+import { IStreamLogger } from '../handlers/stream-logger.interface';
+import { ChildProcessWithoutNullStreams } from 'child_process';
+
+export abstract class CommandExecutor<Input> {
+  constructor(private logger: IStreamLogger) {}
+
+  public async execute() {
+    const input = await this.prompt();
+    const command = this.build(input);
+    const stream = this.spawn(command);
+    this.processStream(stream, this.logger);
+  }
+
+  protected abstract prompt(): Promise<Input>;
+  protected abstract build(input: Input): ICommandExecutor;
+  protected abstract spawn(
+    command: ICommandExecutor,
+  ): ChildProcessWithoutNullStreams;
+  protected abstract processStream(
+    stream: ChildProcessWithoutNullStreams,
+    logger: IStreamLogger,
+  ): void;
+}
